25e0af153d5d3c38bb761b9e3de88bb6133dbd35
Rick Winfrey <rewinfrey@github.com>
2020-03-18 18:45:35 -0700

    Update example.java

--- example.java
+++ example.java
@@ -7,6 +7,7 @@ public class JavaExample {
         System.out.println("Amount after " + t + " years: "+amount);
     }
     
+    
     public static void main(String args[]) {
     	JavaExample obj = new JavaExample();
     	obj.calculate(2000, 5, .08, 12);
