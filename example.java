28347355f42832de7e2b08af083c38d59638c0e4
Rick Winfrey <rewinfrey@github.com>
2021-11-16 11:07:50 -0800

    Update example.java

--- example.java
+++ example.java
@@ -1,14 +1,14 @@
 public class JavaExample {
 
+    public static void main(String args[]) {
+    	JavaExample obj = new JavaExample();
+    	obj.calculate(2000, 5, .08, 12);
+    }
+    
     public void calculate(int finalpoint, int t, double r, int n) {
         double amount = p * Math.pow(1 + (r / n), n * t);
         double cinterest = amount - p;
         System.out.println("Compound Interest after " + t + " years: "+cinterest);
         System.out.println("Amount after " + t + " years: "+amount);
-    }
-    
-    public static void main(String args[]) {
-    	JavaExample obj = new JavaExample();
-    	obj.calculate(2000, 5, .08, 12);
-    }
+    }    
 }
